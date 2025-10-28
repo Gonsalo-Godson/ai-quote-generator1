c8c9a6fd2143e0b402637a9b1c38024d2d427f7c
Gonsalo-Godson <165775049+Gonsalo-Godson@users.noreply.github.com>
2025-10-28 23:21:39 +0530

    Update main.tf

--- terraform-static-web/main.tf
+++ terraform-static-web/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = ">=3.0.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">=3.0.0"
+    }
   }
   required_version = ">=1.5.0"
 }
@@ -18,16 +22,20 @@ provider "azurerm" {
   tenant_id       = "065d3a74-4585-45ad-933d-de0d78a98f02"
 }
 
-# Unique suffix
+# Generate a unique suffix for the storage account name
 resource "random_integer" "suffix" {
   min = 10000
   max = 99999
 }
 
-# Resource group
+# Resource group (use lifecycle to ignore if already exists)
 resource "azurerm_resource_group" "rg" {
   name     = "aiquote-static-rg"
   location = "East Asia"
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 # Storage account for static web hosting
@@ -37,44 +45,62 @@ resource "azurerm_storage_account" "static_sa" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  allow_blob_public_access = true
 }
 
-# Static website (correct syntax for v4.x)
+# Static website hosting
 resource "azurerm_storage_account_static_website" "website" {
   storage_account_id = azurerm_storage_account.static_sa.id
   index_document     = "index.html"
   error_404_document = "index.html"
 }
 
-# Upload website files
+# Wait a few seconds to ensure $web container is created before uploading blobs
+resource "null_resource" "wait_for_static_site" {
+  depends_on = [azurerm_storage_account_static_website.website]
+
+  provisioner "local-exec" {
+    # cross-platform safe delay (Windows-friendly)
+    command = "powershell -Command \"Start-Sleep -Seconds 10\""
+  }
+}
+
+# Upload website files after the static website container is ready
 resource "azurerm_storage_blob" "index" {
-  name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.static_sa.name
+  depends_on            = [null_resource.wait_for_static_site]
+  name                  = "index.html"
+  storage_account_name  = azurerm_storage_account.static_sa.name
   storage_container_name = "$web"
-  type                   = "Block"
-  source                 = "${path.module}/../index.html"
-  content_type           = "text/html"
+  type                  = "Block"
+  source                = "${path.module}/../index.html"
+  content_type          = "text/html"
 }
 
 resource "azurerm_storage_blob" "style" {
-  name                   = "style.css"
-  storage_account_name   = azurerm_storage_account.static_sa.name
+  depends_on            = [null_resource.wait_for_static_site]
+  name                  = "style.css"
+  storage_account_name  = azurerm_storage_account.static_sa.name
   storage_container_name = "$web"
-  type                   = "Block"
-  source                 = "${path.module}/../style.css"
-  content_type           = "text/css"
+  type                  = "Block"
+  source                = "${path.module}/../style.css"
+  content_type          = "text/css"
 }
 
 resource "azurerm_storage_blob" "script" {
-  name                   = "script.js"
-  storage_account_name   = azurerm_storage_account.static_sa.name
+  depends_on            = [null_resource.wait_for_static_site]
+  name                  = "script.js"
+  storage_account_name  = azurerm_storage_account.static_sa.name
   storage_container_name = "$web"
-  type                   = "Block"
-  source                 = "${path.module}/../script.js"
-  content_type           = "application/javascript"
+  type                  = "Block"
+  source                = "${path.module}/../script.js"
+  content_type          = "application/javascript"
 }
 
-# Output
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
 output "static_website_url" {
   value = azurerm_storage_account.static_sa.primary_web_endpoint
 }
