36e6e373b69355116088ac138e4099bd5509f21d
Gonsalo-Godson <165775049+Gonsalo-Godson@users.noreply.github.com>
2025-10-28 23:23:34 +0530

    Update main.tf

--- terraform-static-web/main.tf
+++ terraform-static-web/main.tf
@@ -22,19 +22,20 @@ provider "azurerm" {
   tenant_id       = "065d3a74-4585-45ad-933d-de0d78a98f02"
 }
 
-# Generate a unique suffix for the storage account name
+# Generate unique suffix for storage account
 resource "random_integer" "suffix" {
   min = 10000
   max = 99999
 }
 
-# Resource group (use lifecycle to ignore if already exists)
+# Resource group (ignore if already exists)
 resource "azurerm_resource_group" "rg" {
   name     = "aiquote-static-rg"
   location = "East Asia"
 
   lifecycle {
-    ignore_changes = all
+    prevent_destroy = true
+    ignore_changes  = all
   }
 }
 
@@ -48,59 +49,65 @@ resource "azurerm_storage_account" "static_sa" {
   allow_blob_public_access = true
 }
 
-# Static website hosting
+# Enable static website feature
 resource "azurerm_storage_account_static_website" "website" {
   storage_account_id = azurerm_storage_account.static_sa.id
   index_document     = "index.html"
   error_404_document = "index.html"
 }
 
-# Wait a few seconds to ensure $web container is created before uploading blobs
+# Wait to ensure $web container is available
 resource "null_resource" "wait_for_static_site" {
   depends_on = [azurerm_storage_account_static_website.website]
 
   provisioner "local-exec" {
-    # cross-platform safe delay (Windows-friendly)
     command = "powershell -Command \"Start-Sleep -Seconds 10\""
   }
 }
 
-# Upload website files after the static website container is ready
+# Upload static files
 resource "azurerm_storage_blob" "index" {
-  depends_on            = [null_resource.wait_for_static_site]
-  name                  = "index.html"
-  storage_account_name  = azurerm_storage_account.static_sa.name
+  depends_on             = [null_resource.wait_for_static_site]
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.static_sa.name
   storage_container_name = "$web"
-  type                  = "Block"
-  source                = "${path.module}/../index.html"
-  content_type          = "text/html"
+  type                   = "Block"
+  source                 = "${path.module}/../index.html"
+  content_type           = "text/html"
 }
 
 resource "azurerm_storage_blob" "style" {
-  depends_on            = [null_resource.wait_for_static_site]
-  name                  = "style.css"
-  storage_account_name  = azurerm_storage_account.static_sa.name
+  depends_on             = [null_resource.wait_for_static_site]
+  name                   = "style.css"
+  storage_account_name   = azurerm_storage_account.static_sa.name
   storage_container_name = "$web"
-  type                  = "Block"
-  source                = "${path.module}/../style.css"
-  content_type          = "text/css"
+  type                   = "Block"
+  source                 = "${path.module}/../style.css"
+  content_type           = "text/css"
 }
 
 resource "azurerm_storage_blob" "script" {
-  depends_on            = [null_resource.wait_for_static_site]
-  name                  = "script.js"
-  storage_account_name  = azurerm_storage_account.static_sa.name
+  depends_on             = [null_resource.wait_for_static_site]
+  name                   = "script.js"
+  storage_account_name   = azurerm_storage_account.static_sa.name
   storage_container_name = "$web"
-  type                  = "Block"
-  source                = "${path.module}/../script.js"
-  content_type          = "application/javascript"
+  type                   = "Block"
+  source                 = "${path.module}/../script.js"
+  content_type           = "application/javascript"
 }
 
 # Outputs
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  description = "Azure Resource Group Name"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  description = "Storage Account Name"
+  value       = azurerm_storage_account.static_sa.name
 }
 
 output "static_website_url" {
-  value = azurerm_storage_account.static_sa.primary_web_endpoint
+  description = "URL of the deployed static website"
+  value       = azurerm_storage_account.static_sa.primary_web_endpoint
 }
